20344b4b2679cca1957a15032a44cbcb49f28b03
ash-R2D2 <avinavashish008@gmail.com>
2021-06-22 15:21:24 +0530

    Update TimeTable_Problem.py
    minor bug fixes

--- TimeTable_Problem.py
+++ TimeTable_Problem.py
@@ -4,12 +4,14 @@
 
 import random
 
-# The generate_courses() generates random binary strings represnting different 
+# The generate_courses() generates random binary strings representing different
 # courses based on the total number of courses specified.
 # IF total courses are 8 then it will generate 4 bit numbers. If 17 courses then it generates 
 # 5 bit numbers but none of them will have value more than 17 or less than 1.
 
-def generate_courses(course_count):
+course_count = 0
+def generate_courses():
+    global course_count
     bit_length = len(bin(course_count))-2
     
     n = random.randint(1, course_count)
@@ -20,8 +22,10 @@ def generate_courses(course_count):
 
 
 # A function that generates the genome of specified length using generate courses function.
-def generate_genome(days , slots, course_count):
-    return [generate_courses(course_count) for i in range(days*slots)]
+def generate_genome(genome_length, no_courses):
+    global course_count
+    course_count = no_courses
+    return [generate_courses() for i in range(genome_length)]
 
 
 # x = generate_genome(5,2,17),

d6d2cc38ab8188177b081b032404c13daff5a3ef
Dipan Nanda <dipangithub@gmail.com>
2021-06-26 17:30:30 +0530

    Base: Crossover new logic

--- base.py
+++ base.py
@@ -13,23 +13,21 @@ def single_point_crossover(gene_a, gene_b):
     # Now we choose the point/location for single point crossover which will be a random index
     # between 2nd element and the second last element.
 
-    c = random.choice([1, 2, 3])
     course_bits = len(bin(course_count)) - 2
     slot_bits = len(bin(total_slots)) - 2
     c = random.choice([1, 2, 3])
     if c == 1:
-        gene[0:course_bits] = encode_module()
+        gene_a[0:course_bits], gene_b[0:course_bits] = gene_b[0:course_bits], gene_a[0:course_bits]
     elif c == 2:
-        gene[course_bits:slot_bits] = encode_slot()
+        gene_a[course_bits:slot_bits], gene_b[course_bits:slot_bits] = \
+            gene_b[course_bits:slot_bits], gene_a[course_bits:slot_bits]
     elif c == 3:
-        gene[slot_bits:] = encode_class()
-
+        gene_a[slot_bits:], gene_b[slot_bits:] = gene_b[slot_bits:], gene_a[slot_bits:]
 
     return gene_a, gene_b
 
 
 def multi_point_crossover(gene_a, gene_b, points):
-
     # We use the single point crossover, multiple times
     for _ in range(points):
         gene_a, gene_b = single_point_crossover(gene_a, gene_b)
@@ -38,8 +36,8 @@ def multi_point_crossover(gene_a, gene_b, points):
 
 
 def mutation(gene):
-    #global course_count
-    #global total_slots
+    # global course_count
+    # global total_slots
 
     course_bits = len(bin(course_count)) - 2
     slot_bits = len(bin(total_slots)) - 2
@@ -67,15 +65,14 @@ def sort_population(population):
 
 
 def run_evolution(
-    class_count,
-    total_days,
-    slots,
-    no_courses,
-    population_size,
-    max_fitness,
-    max_generations=100,
+        class_count,
+        total_days,
+        slots,
+        no_courses,
+        population_size,
+        max_fitness,
+        max_generations=100,
 ):
-
     initialize_genotype(class_count, no_courses, slots, total_days)
     population = generate_population(population_size)
     for _ in range(max_generations):

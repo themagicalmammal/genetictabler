1534d3be0be54f5b688911784087dcb605f35e06
Dipan Nanda <dipangithub@gmail.com>
2021-06-30 01:34:31 +0530

    Library: 1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 DESCRIPTION = 'Time Table scheduling using genetic algorithm.'
 LONG_DESCRIPTION = 'Time Table scheduling using with a modified genetic algorithm which is tailored as per the requirments.'
 

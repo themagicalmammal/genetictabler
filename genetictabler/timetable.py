4de3d42610e81054b77289a7e16cddf78cb41619
Dipan Nanda <dipangithub@gmail.com>
2021-07-02 12:16:14 +0530

    Library: Fixed a minor mistake

--- genetictabler/timetable.py
+++ genetictabler/timetable.py
@@ -150,29 +150,26 @@ def calculate_fitness(gene):
     class_no = int(gene[course_bits + slot_bits:], 2)
 
     if tables[class_no - 1][day_no - 1][slot_no - 1] != 0:
-        fitness_score *= 0.01
+        fitness_score *= 0.1
 
     for i in range(class_count):
         if tables[i][day_no - 1][slot_no - 1] == course:
             fitness_score *= 0.6
 
-    if slot_no != 1 and tables[class_no - 1][day_no - 1][slot_no -
-                                                         1] == course:
+    if slot_no != 1 and tables[class_no - 1][day_no - 1][slot_no - 2] == course:
         fitness_score *= 0.6
 
-    if (slot_no != slot_count
-            and tables[class_no - 1][day_no - 1][slot_no - 1] == course):
+    if slot_no != slot_count and tables[class_no - 1][day_no - 1][slot_no] == course:
         fitness_score *= 0.6
 
     if course_quota[class_no - 1][course - 1] < 1:
-        fitness_score *= 0.1
-
-    if (tables[class_no - 1][day_no - 1].count(course) >=
-            repeat_quota[class_no - 1][course - 1]):
         fitness_score *= 1
 
+    if tables[class_no - 1][day_no - 1].count(course) >= repeat_quota[class_no - 1][course - 1]:
+        fitness_score *= 0.1
+
     if teacher_quota[day_no - 1][slot_no - 1][course - 1] == 0:
-        fitness_score *= 0.01
+        fitness_score *= 0.1
     return fitness_score
 
 

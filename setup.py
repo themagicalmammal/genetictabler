e9225b15270014f1ddf2648aa2c8ca493228ddbf
Dipan Nanda <dipangithub@gmail.com>
2021-08-07 11:48:38 +0530

    PyPi: Necessary changes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 DESCRIPTION = 'Time Table scheduling using Genetic Algorithm.'
 LONG_DESCRIPTION = 'Time Table scheduling using Genetic Algorithm which is tailored to work such that resource utilization is reduced in accordance with the genes that are being generated.'
 
@@ -24,7 +24,7 @@ setup(
     install_requires=[],
     keywords=['python', 'geneticalgorithm', 'genetics', 'genes', 'chromosomes', 'timetable'],
     classifiers=[
-        "Development Status :: 1 - Stable",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",

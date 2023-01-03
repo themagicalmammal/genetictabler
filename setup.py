43690ebb4fcb7a70a875169f64f9d255cd741324
Dipan Nanda <43346471+themagicalmammal@users.noreply.github.com>
2023-01-03 15:13:43 +0000

    Bunch of Setup things

--- setup.py
+++ setup.py
@@ -22,7 +22,13 @@ setup(
     long_description=LONG_DESCRIPTION,
     url="https://github.com/themagicalmammal/genetictabler",
     packages=find_packages(),
+    include_package_data=True,
+    license="GPLv3",
     install_requires=[],
+    project_urls={"Website": "https://github.com/themagicalmammal/genetictabler",
+            "Source code": "https://github.com/themagicalmammal/genetictabler",
+            "Documentation": "https://github.com/themagicalmammal/genetictabler/blob/main/README.md",
+            "Bug tracker":   "https://github.com/themagicalmammal/genetictabler/issues"},
     keywords=[
         "python",
         "geneticalgorithm",
@@ -33,10 +39,12 @@ setup(
     ],
     classifiers=[
         "LICENSE :: OSI APPROVED :: GNU GENERAL PUBLIC LICENSE V3 (GPLV3)",
-        "LICENSE :: OSI APPROVED :: GNU GENERAL PUBLIC LICENSE V3 OR LATER (GPLV3+)",
+        "LICENSE :: OSI APPROVED :: GNU GENERAL PUBLIC LICENSE V3 OR LATER (GPLV3+)",          
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",

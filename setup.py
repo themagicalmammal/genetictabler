b5e32d9ee06f98f04fca48b54c2b3a00ccabdf73
Dipan Nanda <43346471+themagicalmammal@users.noreply.github.com>
2023-01-03 15:36:51 +0000

    1.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     LONG_DESCRIPTION = "\n" + fh.read()
 
-VERSION = "1.0.8"
+VERSION = "1.1.0"
 DESCRIPTION = "A time table scheduler using Genetic algorithm(s)."
 
 # Setting up
@@ -22,13 +22,13 @@ setup(
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
-    url="https://github.com/themagicalmammal/genetictabler",
+    # url="https://github.com/themagicalmammal/genetictabler",
     packages=find_packages(),
     include_package_data=True,
     license="GPLv3",
     install_requires=[],
     project_urls={
-        "Website": "https://github.com/themagicalmammal/genetictabler",
+      # "Website": "https://github.com/themagicalmammal/genetictabler",
         "Source code": "https://github.com/themagicalmammal/genetictabler",
         "Documentation":
         "https://github.com/themagicalmammal/genetictabler/blob/main/README.md",
@@ -45,10 +45,9 @@ setup(
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: System Administrators",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
